c0c47548b99fbfdac633f010d6fa2e58765b585e
Sonyah Park <33458738+sonyah2080@users.noreply.github.com>
2026-08-10 14:33:03 +0900

    Update severance.js

--- severance.js
+++ severance.js
@@ -193,6 +193,6 @@ function downloadPdf() {
     html2canvas:  { scale: 2, useCORS: true },
     jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
   };
-
+  
   html2pdf().set(opt).from(element).save();
 }
